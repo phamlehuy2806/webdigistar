6108bd94559db45912c9f906b60655b00d20b7bf
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2021-01-07 13:10:58 +0700

    add tailwindcss framework

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const merge = require('webpack-merge');
+
+module.exports = (config) => {
+  const isProd = config.mode === "production";
+  const tailwindConfig = require("./tailwind.config.js")(isProd);
+
+  return merge(config, {
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            syntax: 'postcss-scss',
+            plugins: () => [
+              require('postcss-import'),
+              require('tailwindcss')(tailwindConfig),
+              require('autoprefixer'),
+            ]
+          }
+        }
+      ]
+    }
+  });
+};
